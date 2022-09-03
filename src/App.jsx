09eddd54b2c92c1639c8fa95f47d9ac5703448e3
Dogun Oghenekobiruo <dogunfx@gmail.com>
2022-09-02 17:31:55 -0700

    first commit for this repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header";
+import Button from "./mybutton";
+import "./App.css";
+
+export default function MyApp() {
+  return (
+    <div>
+      <Header />
+      <Button />
+      <button>Hello</button>
+    </div>
+  );
+}
---
+++ src/mybutton.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState } from "react";
+import styles from "./button.module.css";
+
+export default function({ disabled }) {
+  const [count, setCount] = useState(0);
+
+  var ob1 = {
+    name: "john",
+    age: 78,
+  };
+
+  const handleClick = () => {
+    var newCount = count + 1;
+    setCount(newCount);
+  };
+  return (
+    <button
+      onClick={handleClick}
+      className={!disabled ? styles.button : styles.disabled_button}
+    >
+      Clicked {count} times
+    </button>
+  );
+}
